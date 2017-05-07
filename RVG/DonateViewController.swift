11f9025456f2d29cc266c7d4167de515e3720513
Michael Hanna <mykelkim@yahoo.com>
2017-05-06 23:28:48 -0400

    layout for donate page complete. paypal mock flow complete

--- RVG/DonateViewController.swift
+++ RVG/DonateViewController.swift
@@ -8,14 +8,151 @@
 
 import UIKit
 
-class DonateViewController: BaseClass {
+class DonateViewController: UIViewController, PayPalPaymentDelegate, UITextFieldDelegate {
+    
+    @IBOutlet weak var donationTextField: CurrencyField!
+    @IBOutlet weak var donateButton: UIButton!
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var successMessageLabel: UILabel!
+    @IBOutlet weak var successView: UIView!
+
+    let currencyFormatter = NumberFormatter()
+    
+    var environment:String = PayPalEnvironmentNoNetwork {
+        willSet(newEnvironment) {
+            if (newEnvironment != environment) {
+                PayPalMobile.preconnect(withEnvironment: newEnvironment)
+            }
+        }
+    }
+    
+    var resultText = "" // empty
+    var payPalConfig = PayPalConfiguration() // default
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Donate"
+        self.title = NSLocalizedString("Donate", comment: "")
         self.navigationController?.isNavigationBarHidden=false
+        
+        print("PayPal iOS SDK Version: \(PayPalMobile.libraryVersion())")
+        
+        logoImageView.layer.cornerRadius = logoImageView.frame.size.height / 2
+        logoImageView.layer.masksToBounds = true
+        logoImageView.layer.borderWidth = 0
 
+        _setupDonateButton()
+        _setupSuccessView()
+        _setupDonationTextField()
+}
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        PayPalMobile.preconnect(withEnvironment: environment)
+        
     }
     
+    func _setupDonateButton() {
+        donateButton.isEnabled = false
+        donateButton.addTarget(self, action: #selector(self.donate(_:)), for: .touchUpInside)
+    }
+
+    func _setupSuccessView() {
+        successMessageLabel.text = NSLocalizedString("Your donation was successful.\nThank you!", comment: "")
+        successView.isHidden = true
+    }
+
+    func _setupDonationTextField() {
+        
+        currencyFormatter.numberStyle = .currency
+//        donationTextField.currencyFormatter().minimum = NSNumber(value: 1)
+//        donationTextField.currencyFormatter().maximum = NSNumber(value: 100000)
+        
+        donationTextField.delegate = self
+        donationTextField.font = UIFont (name: "HelveticaNeue-Light", size: 17)
+        donationTextField.text = "0"
+        donationTextField.didMoveToSuperview()
+//        donationTextField.textAlignment = .center
+//        donationTextField.keyboardType = .numbersAndPunctuation
+        donationTextField.becomeFirstResponder()
+        
+    }
+    
+    @IBAction func donate(_ sender: AnyObject) {
+        print("donate!")
+        // Remove our last completed payment, just for demo purposes.
+        resultText = ""
+
+        print("donationTextField.amount: \(donationTextField.amount)")
+        
+        if donationTextField.amount > 0.0 {
+            let item1 = PayPalItem(name: "KJVRVG Donation", withQuantity: 1, withPrice: NSDecimalNumber(string: String(donationTextField.amount)), withCurrency: "USD", withSku: "KJVRVG-ios-0001")
+            
+            let items = [item1]
+            let subtotal = PayPalItem.totalPrice(forItems: items)
+            let total = subtotal
+            
+            let payment = PayPalPayment(amount: total, currencyCode: "USD", shortDescription: "KJVRVG Donation", intent: .sale)
+            payment.items = items
+            
+            if (payment.processable) {
+                let paymentViewController = PayPalPaymentViewController(payment: payment, configuration: payPalConfig, delegate: self)
+                present(paymentViewController!, animated: true, completion: nil)
+            }
+            else {
+                // This particular payment will always be processable. If, for
+                // example, the amount was negative or the shortDescription was
+                // empty, this payment wouldn't be processable, and you'd want
+                // to handle that here.
+                print("Payment not processalbe: \(payment)")
+            }
+        }
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let currencyField = textField as? CurrencyField {
+            print("textFieldDidEndEditing: \(currencyField)")
+            print("currencyField.amount: \(currencyField.amount)")
+            if currencyField.amount > 0.0 {
+                donateButton.isEnabled = true
+            } else {
+                donateButton.isEnabled = false
+            }
+        }
+    }
+
+    // PayPalPaymentDelegate
+    
+    func payPalPaymentDidCancel(_ paymentViewController: PayPalPaymentViewController) {
+        print("PayPal Payment Cancelled")
+        resultText = ""
+        successView.isHidden = true
+        paymentViewController.dismiss(animated: true, completion: nil)
+    }
+    
+    func payPalPaymentViewController(_ paymentViewController: PayPalPaymentViewController, didComplete completedPayment: PayPalPayment) {
+        print("PayPal Payment Success !")
+        paymentViewController.dismiss(animated: true, completion: { () -> Void in
+            // send completed confirmaion to your server
+            print("Here is your proof of payment:\n\n\(completedPayment.confirmation)\n\nSend this to your server for confirmation and fulfillment.")
+
+            self.resultText = completedPayment.description
+            self.showSuccess()
+        })
+    }
+
+    func showSuccess() {
+        successView.isHidden = false
+        successView.alpha = 1.0
+        UIView.beginAnimations(nil, context: nil)
+        UIView.setAnimationDuration(0.5)
+        UIView.setAnimationDelay(2.0)
+        successView.alpha = 0.0
+        UIView.commitAnimations()
+    }
+
 }
+
+
+//extension ViewController: UITextFieldDelegate {
+//}
 
---
+++ RVG/UIView+Debug.m
@@ -0,0 +1,82 @@
+//
+//  UIView+ConstraintHelpers.m
+//  PractitionerPortal
+//
+//  Created by Kashif Shaikh on 12/22/2013.
+//  Copyright (c) 2013 PointClickCare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UIView+Debug.h"
+
+@implementation UIView (Debug)
++ (void) colorViewsRandomly:(UIView*)view {
+    view.backgroundColor = [UIView randomColor];
+    for (UIView *subview in view.subviews) {
+        [UIView colorViewsRandomly:subview];
+    }
+}
+
++ (void) logViewRect:(UIView*)view level:(NSInteger)level {
+    NSString *indentation = [@"" stringByPaddingToLength:level * 2 withString:@"  " startingAtIndex:0];
+    NSLog(@"%@%@: x:%.0f y:%.0f w:%.0f h:%.0f", indentation, NSStringFromClass(view.class), CGRectGetMinX(view.frame), CGRectGetMinY(view.frame), CGRectGetWidth(view.frame), CGRectGetHeight(view.frame));
+    for (UIView *subview in view.subviews) {
+        [UIView logViewRect:subview level:level + 1];
+    }
+}
+
+
++ (UIColor*)randomColor {
+    CGFloat hue = ( arc4random() % 256 / 256.0 );  //  0.0 to 1.0
+    CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from white
+    CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from black
+    CGFloat alpha = (3 + arc4random() % 5) / 10.0; // 0.3 to 0.7
+    UIColor *color = [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:alpha];
+    return color;
+}
+
+
++ (UIImageView *)writeImageViewForView:(UIView *)view
+{
+    UIGraphicsBeginImageContextWithOptions(view.frame.size, YES, 0.0);
+    [view drawViewHierarchyInRect:view.frame afterScreenUpdates:NO];
+    UIImage *snapshot = UIGraphicsGetImageFromCurrentImageContext();
+    
+    UIGraphicsEndImageContext();
+    UIImageView* imageView = [[UIImageView alloc] initWithImage:snapshot];
+    
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *filePath = [[paths objectAtIndex:0] stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.png", [UIView guid]]];
+    
+#if TARGET_IPHONE_SIMULATOR
+    NSLog(@"file: %@", filePath);
+#endif
+    // Save image.
+    [UIImagePNGRepresentation(imageView.image) writeToFile:filePath atomically:YES];
+
+    return imageView;
+}
+
++ (UIImageView *)writeImageView:(UIImageView *)imageView
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *filePath = [[paths objectAtIndex:0] stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.png", [UIView guid]]];
+    
+#if TARGET_IPHONE_SIMULATOR
+    NSLog(@"file: %@", filePath);
+#endif
+    // Save image.
+    [UIImagePNGRepresentation(imageView.image) writeToFile:filePath atomically:YES];
+    
+    return imageView;
+}
+
++ (NSString*)guid
+{
+    CFUUIDRef theUUID = CFUUIDCreate(NULL);
+    CFStringRef string = CFUUIDCreateString(NULL, theUUID);
+    CFRelease(theUUID);
+    return (__bridge NSString *)(string);
+}
+
+@end
