4d3e10589e6ea52c90df85188d4b223e9f6aae3c
Michael Hanna <mykelkim@yahoo.com>
2017-05-27 17:21:30 -0400

    cleanup about us and privacy policy

--- RVG/AboutUsViewController.swift
+++ RVG/AboutUsViewController.swift
@@ -7,15 +7,16 @@ import UIKit
 
 class AboutUsViewController: BaseClass {
 
-    @IBOutlet weak var txtVw: UITextView!
+    @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.textView.isScrollEnabled = false
+
         self.navigationController?.isNavigationBarHidden=false
         self.title = NSLocalizedString("About Us", comment: "")
         
         let text : String = NSLocalizedString("The Reina Valera Gomez 2010 Audio Bible, This application is available in Spanish and English interfaces.\r\nThis application is the first application available for the Reina Valera Gomez 2010. This application is the beginning of a much greater project. Currently we support the New Testament in Audio, however as we continue to record the Old Testament and eventually  re-record the New Testament we will support both with a Native Speaker. Currently the native speakers helping to see this application come \r\ntogether are Sean Jolley, Israel Flores and Dr. Humberto Gómez. This application will also someday house the full text of the RVG Bible as well as the Full Text of the King James Bible, with the function to read both Bibles on the same page! We also look forward to the day of supporting the New Testament in Audio for the KJV read by Dominique Davis and someday the Old Testament of the KJV. This application is full of promise, and we hope that you enjoy what is just the beginning! The current reader of this application is Collin Schneide, who is also the editor of this New Testament and the coming Native Speakers Testaments. This application is produced in accordance with the copyright on the RVG Bible. Please visit our website at www.kjvrvg.com for more information, and free downloads of the Audio in this application!\r\n\r\nFor the glory of God and his Word Jesus Christ!\r\nThe KJV RVG Team\r\n\r\nPsalm 119:99 I have more understanding than all my teachers: for thy testimonies are my meditation.", comment: "")
-        self.txtVw.text = text
+        self.textView.text = text
 
         // Do any additional setup after loading the view.
     }
@@ -25,9 +26,9 @@ class AboutUsViewController: BaseClass {
         self.navigationController?.isNavigationBarHidden=false
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        self.textView.isScrollEnabled = true
     }
     
     override func viewWillDisappear(_ animated: Bool) {
--- RVG/PrivacyPolicy.swift
+++ RVG/PrivacyPolicy.swift
@@ -6,20 +6,19 @@
 import UIKit
 
 class PrivacyPolicy: BaseClass{
-
     
-    @IBOutlet weak var bodyLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.textView.isScrollEnabled = false
+        
         self.navigationController?.isNavigationBarHidden=false
         
         self.title = NSLocalizedString("Privacy Policy", comment: "")
         
         let text : String = NSLocalizedString("The Reina Valera Gomez 2010 Audio Bible\r\n\r\nAvailable in Spanish and English interfaces\r\n\r\nEase of use: Listen to the RVG 2010 Audio Bible without having to download the massive library of files; each chapter at your finger tips.\r\n\r\nExtras: Get to know the producers, our vision, and what we're doing\r\n\r\nContact us: Get in touch with our Team if you have any great ideas you'd like to share with us, or feedback about our application!\r\n\r\nShare: Tell all your friends about the first every Audio Bible available for the Reina Valera Gomez, the most superior Spanish translation available.\r\n\r\nMusic Player: Play, pause, repeat your favorite tracks, and browse through our library of all the books of the New Testament.\r\n\r\nBonus Track: Do you know for sure if you died today you would go to heaven? Listen to our bonus track and find out how you can know for sure!\r\n\r\nPlease be aware that any information shared through the option \"Contact Us\" is handled solely at the discretion of the owner of the application. This application is produced in according with the Copyright of the RVG which outlines the free distribution of the literature barring the profiting off of the venture, and has the expressed consent of Dr. Gomez. Any electronic property produced by this application is freely given, and may be freely distributed from our website www.kjvrvg.com, subsection Downloads, subsection RVG in Audio.\r\n\r\nFor the glory of God and his Word Jesus Christ!\r\nThe KJV RVG Team", comment: "")
         
-        self.bodyLabel.text = text
-        UIView.colorViewsRandomly(self.view)
+        self.textView.text = text
 
     }
     
@@ -28,9 +27,9 @@ class PrivacyPolicy: BaseClass{
         self.navigationController?.isNavigationBarHidden=false
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        self.textView.isScrollEnabled = true
     }
     
     override func viewWillDisappear(_ animated: Bool) {
