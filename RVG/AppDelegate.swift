3efff9d0d8f644b3a295be85cd6c99075a2b3992
Michael Hanna <mykelkim@yahoo.com>
2017-06-26 22:16:32 -0400

    Support playback in background.

--- RVG/AppDelegate.swift
+++ RVG/AppDelegate.swift
@@ -5,6 +5,7 @@
 
 import UIKit
 import IQKeyboardManagerSwift
+import AVFoundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -22,6 +23,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
 //        PayPalMobile.initializeWithClientIds(forEnvironments: [PayPalEnvironmentProduction: "ARSxhWtMfW3z1YHzWHm9aOfou1xCsIB9qHL8CQZN8u2bkWrt3ta3AEWL8h5XGjz0RD7kjCtk6JQQH5yF",
 //                                                               PayPalEnvironmentSandbox: "Af1eaNhnfWzH0khmBDFYiKBr8JkkCIA2Jft-X77nCPv7EHNTvhf3U7tLJQSE31hgP_9xP1eA3c0HbN8_"])
+        
+        do  {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+        } catch {
+            print("setting AVAudioSessionCategoryPlayback failed")
+        }
         
         return true
     }
