e01006eeac3b37af398c5c900c0ff67662708a13
Michael Hanna <mykelkim@yahoo.com>
2019-08-25 01:21:29 -0400

    share file, share link

--- RVG/AppDependencyInjection/MediaListingViewController.swift
+++ RVG/AppDependencyInjection/MediaListingViewController.swift
@@ -516,8 +516,16 @@ public final class MediaListingViewController: UIViewController, UICollectionVie
                     
                 }))
             }
-            actionController.addAction(Action(ActionData(title: "Share...", image: UIImage(named: "yt-share-icon")!), style: .default, handler: { action in
+            if let fileDownload: FileDownload = downloadedItems[mediaItem.uuid] {
+                actionController.addAction(Action(ActionData(title: "Share File...", image: UIImage(named: "yt-share-icon")!), style: .default, handler: { action in
+                    
+                    self.shareFile(mediaItem: mediaItem)
+                }))
+            }
+            actionController.addAction(Action(ActionData(title: "Share Link...", image: UIImage(named: "yt-share-icon")!), style: .default, handler: { action in
+                self.shareLink(mediaItem: mediaItem)
             }))
+
             actionController.addAction(Action(ActionData(title: "Cancel", image: UIImage(named: "yt-cancel-icon")!), style: .cancel, handler: nil))
             
             present(actionController, animated: true, completion: nil)
@@ -735,5 +743,83 @@ extension MediaListingViewController {
         // GB
         floatSize = floatSize / 1024
         return String(format: "%.1f GB", floatSize)
+    }
+}
+
+extension MediaListingViewController {
+    func shareLink(mediaItem: MediaItem) {
+        if let hashLink: URL = URL(string: "https://api.faithfulword.app/m"),
+            let presenterName: String = mediaItem.presenterName ?? "Unknown Presenter",
+            let shareUrl: URL = hashLink.appendingPathComponent(mediaItem.hashId) {
+            DDLogDebug("hashLink: \(shareUrl)")
+            
+            let message = MessageWithSubjectActivityItem(subject: String(describing: "\(mediaItem.localizedname) by \(presenterName)"), message: "Shared via the Faithful Word App: https://faithfulwordapp.com/")
+            let itemsToShare: [Any] = [message, shareUrl]
+            
+            let activityViewController = UIActivityViewController(activityItems: itemsToShare, applicationActivities: nil)
+            
+            activityViewController.excludedActivityTypes = [
+                .addToReadingList,
+                .openInIBooks,
+                .print,
+                .saveToCameraRoll,
+                .postToWeibo,
+                .postToFlickr,
+                .postToVimeo,
+                .postToTencentWeibo]
+            
+            self.present(activityViewController, animated: true, completion: {})
+        }
+        
+
+    }
+    
+    func shareFile(mediaItem: MediaItem) {
+        // copy file to temp dir to rename it
+        let temporaryDirectoryURL = URL(fileURLWithPath: NSTemporaryDirectory())
+        // generate temp file url path
+        
+        if let presenterName: String = mediaItem.presenterName ?? "Unknown Presenter",
+            let path: String = mediaItem.path,
+            let remoteUrl: URL = URL(string: EnvironmentUrlItemKey.ProductionFileStorageRootUrl.rawValue.appending("/").appending(path)) {
+            
+            let firstPart: String = "\(presenterName.replacingOccurrences(of: " ", with: ""))"
+            let secondPart: String = "\(mediaItem.localizedname.replacingOccurrences(of: " ", with: "")).\(remoteUrl.pathExtension)"
+            let destinationLastPathComponent: String = String(describing: "\(firstPart)-\(secondPart)")
+            
+            let sourceFileUrl: URL = FileSystem.savedDirectory.appendingPathComponent(mediaItem.uuid.appending(String(describing: ".\(remoteUrl.pathExtension)")))
+            let temporaryFileURL: URL = temporaryDirectoryURL.appendingPathComponent(destinationLastPathComponent)
+            DDLogDebug("temporaryFileURL: \(temporaryFileURL)")
+            
+            // capture the audio file as a Data blob and then write it
+            // to temp dir
+            
+            do {
+                let audioData: Data = try Data(contentsOf: sourceFileUrl, options: .uncached)
+                try audioData.write(to: temporaryFileURL, options: .atomicWrite)
+            } catch {
+                DDLogDebug("error writing temp audio file: \(error)")
+                return
+            }
+            
+            let message = MessageWithSubjectActivityItem(subject: String(describing: "\(mediaItem.localizedname) by \(presenterName)"), message: "Shared via the Faithful Word App: https://faithfulwordapp.com/")
+            let itemsToShare: [Any] = [message, temporaryFileURL]
+            
+            let activityViewController = UIActivityViewController(activityItems: itemsToShare, applicationActivities: nil)
+            
+            activityViewController.excludedActivityTypes = [
+                .addToReadingList,
+                .openInIBooks,
+                .print,
+                .saveToCameraRoll,
+                .postToWeibo,
+                .postToFlickr,
+                .postToVimeo,
+                .postToTencentWeibo]
+            
+            self.present(activityViewController, animated: true, completion: {})
+        }
+        
+
     }
 }
